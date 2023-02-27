9fe68585cb12f93abdbd4ccc320552d07e858b2f
mdorici <marianadorici@gmail.com>
2023-02-26 22:00:53 -0300

    Adding React Router DOM

--- src/components/ButtonInverse/index.tsx
+++ src/components/ButtonInverse/index.tsx
@@ -1,10 +1,10 @@
-import './styles.css';
+import { ButtonDTO } from "../../models/button";
+import "./styles.css";
 
-export default function ButtonInverse() {
-    return (
-        <div className="dsc-btn dsc-btn-white">
-            Início
-        </div>
-    );
+type Props = {
+  text: string;
+};
+
+export default function ButtonInverse({ text }: Props) {
+  return <div className="dsc-btn dsc-btn-white">{text}</div>;
 }
-

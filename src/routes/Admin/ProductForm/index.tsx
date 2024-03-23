7d370523db64455edea7a0db6a8ae82d329659a4
mdorici <marianadorici@gmail.com>
2024-03-22 23:46:17 -0300

    FormTextArea

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import FormInput from "../../../components/FormInput";
 import * as forms from "../../../utils/forms";
 import * as productService from "../../../services/product-service";
+import FormTextArea from "../../../components/FormTextArea";
 
 export default function ProductForm() {
 
@@ -32,7 +33,7 @@ export default function ProductForm() {
       validation: function(value: any) {
         return Number(value) > 0;
       },
-      message:"Favor informar um valor positivo.",
+      message:"Favor informar um valor positivo",
     },
     imgUrl: {
       value: "",
@@ -40,6 +41,17 @@ export default function ProductForm() {
       name: "imgUrl",
       type: "text",
       placeholder: "Imagem",
+    },
+    description: {
+      value: "",
+      id: "description",
+      name: "description",
+      type: "text",
+      placeholder: "Descrição",
+      validation: function(value: string) {
+        return /^.{10,}$/.test(value);
+      },
+      message: "A descrição deve ter pelo menos 10 caracteres"
     },
   });
 
@@ -93,6 +105,15 @@ export default function ProductForm() {
                   onTurnDirty={handleTurnDirty}
                   onChange={handleInputChange}
                 />
+              </div>
+              <div>
+                <FormTextArea
+                  {...formData.description}
+                  className="dsc-form-control dsc-textarea"
+                  onTurnDirty={handleTurnDirty}
+                  onChange={handleInputChange}
+                />
+                <div className="dsc-form-error">{formData.description.message}</div>
               </div>
             </div>
             <div className="dsc-product-form-buttons">
